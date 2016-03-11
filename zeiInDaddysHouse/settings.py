bcc30ac65f0ca995bc02aa1d9cda86e92f8f77c2
Jingda Chen <holaelmundokingston@gmail.com>
2016-03-10 20:46:31 -0800

    directory adjusted

--- zeiInDaddysHouse/settings.py
+++ zeiInDaddysHouse/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home',
+    'bbjjzl',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
